48a454862fe0578dd0c0d7ac6459a81b876845c9
Yurik Hayrapetyan <yuhayrap@c1r2s1.42yerevan.am>
2025-05-24 19:47:31 +0400

    fix avatar

--- backend/src/utils/avatar.js
+++ backend/src/utils/avatar.js
@@ -1,9 +1,9 @@
 function createRandomProfileImage(userIdentifier) {
-	const sanitizedIdentifier = userIdentifier.replace(/[^a-zA-Z0-9]/g, '');
-	const availableImageSets = [1, 2, 3, 5];
-	const selectedSet = `set${availableImageSets[Math.floor(Math.random() * availableImageSets.length)]}`;
-	const backgroundStyle = `bg${Math.floor(Math.random() * 2) + 1}`;
-	return `${process.env.DEFAULT_AVATAR}${encodeURIComponent(sanitizedIdentifier)}?set=${selectedSet}&bgset=${backgroundStyle}`;
+    const sanitizedIdentifier = userIdentifier.replace(/[^a-zA-Z0-9]/g, '');
+
+    const selectedStyle = 'pixel-art';
+
+    return `${process.env.DEFAULT_AVATAR}${selectedStyle}/svg?seed=${encodeURIComponent(sanitizedIdentifier)}`;
 }
 
-export { createRandomProfileImage }
+export { createRandomProfileImage };
